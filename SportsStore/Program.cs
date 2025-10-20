4e1d91b5486c042aa2ccb7017a8df9073daede4e
Ali Al_Razaj <alialr09646.09433@gmail.com>
2025-10-20 22:07:01 +0300

    Creating The Project - Adding a data model and support for a database - Paginating data

---
+++ SportsStore/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StoreDbContext>
+(
+    opts =>
+    {
+        opts.UseSqlServer
+        (
+            builder.Configuration["ConnectionStrings:SportsStoreConnection"]
+        );
+    }
+);
+
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+
+WebApplication app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.UseStaticFiles();
+app.MapControllerRoute("pagination", "Products/Page{productPage}", new { Controller = "Home", action = "Index" });
+app.MapDefaultControllerRoute();
+
+SeedData.EnsurePopulated(app);
+
+app.Run();
