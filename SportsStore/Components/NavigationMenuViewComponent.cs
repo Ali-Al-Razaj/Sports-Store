3c9ba9faacc6baea21be78130f2d60d1bba33851
Ali Al_Razaj <alialr09646.09433@gmail.com>
2025-10-21 22:18:25 +0300

    Adding navigation controls - Building a category navigation menu

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components;
+
+public class NavigationMenuViewComponent : ViewComponent
+{
+    private IStoreRepository repository;
+
+    public NavigationMenuViewComponent(IStoreRepository repo)
+    {
+        this.repository = repo;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        ViewBag.SelectedCategory = RouteData?.Values["category"];
+        return View(repository.Products.Select(p => p.Category).Distinct().OrderBy(x => x));
+    }
+
+}
