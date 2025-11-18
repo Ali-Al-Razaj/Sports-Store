fafb0ca398dd78aea935edbae8eda2b5e7eb3b39
Ali Al_Razaj <alialr09646.09433@gmail.com>
2025-11-18 04:02:51 +0300

    Submitting orders - Implementing order processing

--- SportsStore/Models/StoreDbContext.cs
+++ SportsStore/Models/StoreDbContext.cs
@@ -8,5 +8,6 @@ public class StoreDbContext : DbContext
     public StoreDbContext(DbContextOptions<StoreDbContext> options)
         : base(options) { }
     public DbSet<Product> Products => Set<Product>();
+    public DbSet<Order> Orders => Set<Order>();
 
 }
